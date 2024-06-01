e8ffbf771d8fee0df973340581b6b47f211bfb6a
Yrh7383111 <Yrh7383111@outlook.com>
2024-06-01 15:23:54 -0700

    feat: start auth

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -12,7 +12,22 @@ final class AuthManager {
     static let shared = AuthManager()
     
     
+    struct Constants {
+        static let clientID = "63dd489b94cd4b098ff21ac2c3ee502c"
+        static let clientSecret = "fa7f44178d384ba8b33859958c2f2129"
+    }
+    
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let base = "https://accounts.spotify.com/authorize"
+        let redirectURI = "https://courses.iosacademy.io"
+        let scopes = "user-read-private"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -32,5 +47,18 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        
+    }
+    
+    private func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
