6d25457cf4ac95f8202daa7b6e33738ac06f1c12
Yrh7383111 <Yrh7383111@outlook.com>
2024-06-01 16:05:41 -0700

    feat: continue auth

--- Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Controllers/Other/AuthViewController.swift
@@ -42,4 +42,22 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         
         webView.frame = view.bounds
     }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else {
+            return
+        }
+        guard let code = URLComponents(string: url.absoluteString)?.queryItems?.first(where: { $0.name == "code" })?.value else {
+            return
+        }
+        webView.isHidden = true
+        
+        print("Code: \(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+        }
+    }
 }
