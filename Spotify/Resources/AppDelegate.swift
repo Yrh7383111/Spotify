ce6145d774925caea99ddfe2f717e22eecab3475
Yrh7383111 <Yrh7383111@outlook.com>
2024-06-02 16:24:31 -0700

    feat: complete auth

--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -27,6 +27,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         window.makeKeyAndVisible()
         self.window = window
+        
+        AuthManager.shared.refreshIfNeeded { success in
+            print(success)
+        }
                 
         return true
     }
