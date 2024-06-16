87a25fa586fba9145e21bc3cea233196e0d75f6a
Yrh7383111 <Yrh7383111@outlook.com>
2024-06-16 16:29:38 -0700

    feat: complete profile

--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
+        
         if AuthManager.shared.isSignedIn {
+            AuthManager.shared.refreshIfNeeded(completion: nil)
             window.rootViewController = TabBarViewController()
         }
         else {
@@ -25,12 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
             window.rootViewController = navVC
         }
+        
         window.makeKeyAndVisible()
         self.window = window
-        
-        AuthManager.shared.refreshIfNeeded { success in
-            print(success)
-        }
                 
         return true
     }
