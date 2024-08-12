02aff6bfb9c4a82d17231a4e7d4facca71f05007
Yrh7383111 <Yrh7383111@outlook.com>
2024-08-11 17:15:29 -0700

    [Playlists & Albums] feat: create playlists

---
+++ Spotify/Presenter/PlaybackPresenter.swift
@@ -0,0 +1,11 @@
+//
+//  PlaybackPresenter.swift
+//  Spotify
+//
+//  Created by Yin, Jackson on 2024-08-11.
+//
+
+
+import Foundation
+
+
---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Yin, Jackson on 2024-08-11.
+//
+
+
+import Foundation
+
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
