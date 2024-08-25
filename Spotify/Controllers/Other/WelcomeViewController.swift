f245de391b90967d0e0803517462f124f3bf6040
Yrh7383111 <Yrh7383111@outlook.com>
2024-08-24 21:54:34 -0700

    [Welcome] feat: add weclome screen

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -5,68 +5,132 @@
 //  Created by Yin, Jackson on 2024-05-31.
 //
 
+
 import UIKit
 
 
-class WelcomeViewController: UIViewController {
 
+class WelcomeViewController: UIViewController {
+    
+    // MARK: - Variable
     private let signInButton: UIButton = {
         let button = UIButton()
+        
         button.backgroundColor = .white
-        button.setTitle("Sign in with Spotify", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.black, for: .normal)
         
         return button
     }()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        title = "Spotify"
-        view.backgroundColor = .systemGreen
-        view.addSubview(signInButton)
-        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
-    }
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "albums_background")
+        
+        return imageView
+    }()
+
+    private let overlayView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .black
+        view.alpha = 0.7
+        
+        return view
+    }()
+
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.text = "Listen to Millions\nof Songs on\nthe go."
+        
+        return label
+    }()
+
     
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        title = "Spotify"
+        view.addSubview(imageView)
+        view.addSubview(overlayView)
+        view.backgroundColor = .blue
+        view.addSubview(signInButton)
+        
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
+        view.addSubview(label)
+        view.addSubview(logoImageView)
+    }
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        imageView.frame = view.bounds
+        overlayView.frame = view.bounds
         signInButton.frame = CGRect(
             x: 20,
-            y: view.height - 50 - view.safeAreaInsets.bottom,
-            width: view.width - 40,
+            y: view.height-50-view.safeAreaInsets.bottom,
+            width: view.width-40,
             height: 50
         )
+
+        logoImageView.frame = CGRect(
+            x: (view.width-120)/2,
+            y: (view.height-350)/2,
+            width: 120,
+            height: 120
+        )
+        label.frame = CGRect(
+            x: 30,
+            y: logoImageView.bottom+30,
+            width: view.width-60,
+            height: 150
+        )
     }
-    
+
     @objc func didTapSignIn() {
         let vc = AuthViewController()
         vc.completionHandler = { [weak self] success in
             DispatchQueue.main.async {
                 self?.handleSignIn(success: success)
             }
-            
         }
+        
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
-    
+
     private func handleSignIn(success: Bool) {
         guard success else {
-            let alert = UIAlertController(title: "Oops",
-                                          message: "Somethign went wrong when signing in.",
-                                          preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Dismiss",
-                                          style: .cancel,
-                                          handler: nil))
+            let alert = UIAlertController(
+                title: "Oops",
+                message: "Something went wrong when signing in.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
             present(alert, animated: true)
             
             return
         }
-        
+
         let mainAppTabBarVC = TabBarViewController()
         mainAppTabBarVC.modalPresentationStyle = .fullScreen
         present(mainAppTabBarVC, animated: true)
     }
 }
-
